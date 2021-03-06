ccf256d6d565a8fc35553fdf5a414e883258e886
ksh <hyun031916@gmail.com>
2021-03-06 23:39:36 +0900

    API 부르는 클래스 생성

--- app/src/main/java/com/example/weatherdustchecker/MainActivity.kt
+++ app/src/main/java/com/example/weatherdustchecker/MainActivity.kt
@@ -8,7 +8,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.hide()
+
         val manager = supportFragmentManager
         val transaction = manager.beginTransaction()
+
+        /*
+        transaction.add(R.id.fragment_container,
+            WeatherPageFragment.newInstance(37.58, 126.98))
+        */
+        transaction.add(R.id.fragment_container,
+                DustPageFragment.newInstance(37.58, 126.98))
+
+        transaction.commit()
     }
 }
